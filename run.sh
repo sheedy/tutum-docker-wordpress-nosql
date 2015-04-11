446ad4c856056d9508063f764d23d03581dfb53f
Michael Sheedy <sheedy@users.noreply.github.com>
2015-04-11 02:32:16 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,6 +20,41 @@ echo "      Database Name:          $DB_NAME"
 echo "      Database Username:      $DB_USER"
 echo "========================================================================"
 
+# Volume script from https://github.com/romaninsh/docker-wordpress/blob/master/volume-init.sh
+
+test -d /data/ || { echo "No data volume found. Skipping."; }
+
+# Grab wp-content if it's found (plugins etc)
+if [ ! -d /data/wp-content/ ]; then
+  echo "Moving wp-content to blank volume.."
+  cp -aR /app/wp-content/ /data/
+fi
+
+echo "Linking wp-content.."
+rm -rf /app/wp-content
+ln -sf /data/wp-content /app/wp-content
+
+# Grab custom config file if it's there
+if [ -f /data/wp-config-production.php ]; then
+  echo "Using wp-config-production.php.."
+  rm -f /app/wp-config.php
+  ln -sf /data/wp-config-production.php /app/wp-config.php
+fi
+
+# Grab .htaccess if it's there
+if [ -f /data/.htaccess ]; then
+  echo "Using .htaccess.."
+  ln -sf /data/.htaccess /app/.htaccess
+fi
+
+# Execute init.sh if it's found
+if [ -x /data/init.sh ]; then
+  echo "Executing custom init.sh.."
+  /data/init.sh
+fi
+
+# End volume script
+
 if [ -f /.mysql_db_created ]; then
         exec /run.sh
         exit 1
@@ -62,39 +97,6 @@ else
 fi
 
 touch /.mysql_db_created
-
-# Volume script from https://github.com/romaninsh/docker-wordpress/blob/master/volume-init.sh
-
-test -d /data/ || { echo "No data volume found. Skipping."; }
-
-# Grab wp-content if it's found (plugins etc)
-if [ ! -d /data/wp-content/ ]; then
-  echo "Moving wp-content to blank volume.."
-  cp -aR /app/wp-content/ /data/
-fi
-
-echo "Linking wp-content.."
-rm -rf /app/wp-content
-ln -sf /data/wp-content /app/wp-content
-
-# Grab custom config file if it's there
-if [ -f /data/wp-config-production.php ]; then
-  echo "Using wp-config-production.php.."
-  rm -f /app/wp-config.php
-  ln -sf /data/wp-config-production.php /app/wp-config.php
-fi
-
-# Grab .htaccess if it's there
-if [ -f /data/.htaccess ]; then
-  echo "Using .htaccess.."
-  ln -sf /data/.htaccess /app/.htaccess
-fi
-
-# Execute init.sh if it's found
-if [ -x /data/init.sh ]; then
-  echo "Executing custom init.sh.."
-  /data/init.sh
-fi
 
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
